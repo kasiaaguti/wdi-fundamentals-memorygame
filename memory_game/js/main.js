ae90db03f79d9de68505ba0e0040b5135cc3f01c
Katarzyna Wypychewicz <katarzynawypychewicz@katarzynas-air.home>
2019-05-02 22:09:44 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,24 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay =[];
+
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("you found a match!");
+} else {
+	alert("Sorry, try again");
+ }
+
+
+
+
+  
